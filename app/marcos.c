0a64f09bc341c80913e9506c0cfcc8cb0440c30f
Marcos <marcos.fierros@addadev.com>
2020-05-17 19:24:20 -0500

    Added Random Targets

--- app/marcos.c
+++ app/marcos.c
@@ -2,6 +2,8 @@
 #include <GL/freeglut.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "Transforms.h"
 #include "Utils.h"
 #include "Cylinder.h"
@@ -11,6 +13,8 @@
 
 #define toRadians(deg) deg * M_PI / 180.0
 #define max(a, b) a > b ? a : b
+#define N_PILLAR 6
+#define N_TARGETS 15
 
 static const float ROOM_WIDTH  = 40;
 static const float ROOM_HEIGHT = 6;
@@ -25,12 +29,23 @@ static MOTION_TYPE motionType = 0;
 
 static float observerX = 0;
 static float observerY = 0;
-static float observerZ = 0;
+static float observerZ = 18;
 static float observerYaw = 0;
 static float observerPitch = 0;
 static float observerSpeed = 0.1;
+static float observerR = 2.0;
 static float angleSpeed = 0.1;
 static int mouseX, mouseY;
+static float timer2sec = 0;
+static int currentTargets = 0;
+
+typedef struct {
+    Vec3 position;
+    Vec3 color;
+    bool shot;
+} Target;
+
+Target targets[N_TARGETS];
 
 static Mat4   modelMatrix, projectionMatrix, viewMatrix;
 
@@ -48,7 +63,7 @@ static GLuint programId4, vertexPositionLoc4;
 static GLuint ambientLightLoc, materialALoc, materialDLoc;
 static GLuint materialSLoc, cameraPositionLoc;
 
-static GLuint roomVA, playerVA, crossVA;
+static GLuint roomVA, playerVA, crossVA, targetVA;
 
 static vec3 ambientLight  = {0.5, 0.5, 0.5};
 
@@ -56,7 +71,6 @@ static vec3 materialA     = {0.8, 0.8, 0.8};
 static vec3 materialD     = {0.6, 0.6, 0.6};
 static vec3 materialS     = {0.6, 0.6, 0.6};
 
-//                          Color    subcutoff,  Position  Exponent Direction  Cos(cutoff)
 static float lights[]   = {
         1, 1, 0.93, 	// Color
         0.9238,    		// Sub-cutoff
@@ -95,6 +109,15 @@ static vec3 bulletPosition = {0,0,0};
 static vec3 bulletDirection = {0,0,0};
 static int shootActive = 0, bulletInRange = 1;
 
+typedef struct {
+    Vec3 pos;
+    float r;
+    float h;
+    Bool shot;
+} Pillar;
+
+Pillar pillars [N_PILLAR] ;
+
 static void initTexture(const char* filename, GLuint textureId) {
     unsigned char* data;
     unsigned int width, height;
@@ -112,6 +135,7 @@ static void initTextures() {
     initTexture("textures/Brick.bmp",    textures[0]);
     initTexture("textures/Ceiling.bmp",  textures[1]);
     initTexture("textures/Gun.bmp",  textures[2]);
+    initTexture("textures/Target.bmp",  textures[3]);
 }
 
 static int initShaders() {
@@ -272,6 +296,8 @@ static void initRoom() {
     glEnableVertexAttribArray(vertexTexcoordLoc);
 }
 
+
+
 static void initPlayer() {
     float positions[] =  { 0.0, -0.2,    0.0, -1,    0.5, -0.2,    0.5, -1 };
     float colors[] = { 1, 1, 1,  1, 1, 1, 1, 1, 1,  1, 1, 1};
@@ -303,7 +329,7 @@ static void initCross() {
     int w = glutGet(GLUT_WINDOW_WIDTH);
     int h = glutGet(GLUT_WINDOW_HEIGHT);
     float aspect = (float)w / h;
-    printf("%d, %d %.4f\n", w, h, aspect);
+//    printf("%d, %d %.4f\n", w, h, aspect);
     float scale = 0.05;
     float model [] = {
             0,  		1 * scale,
@@ -324,20 +350,138 @@ static void initCross() {
     glEnableVertexAttribArray(vertexPositionLoc4);
 }
 
+static void initPillars() {
+    for(int i = 0; i < N_PILLAR; i++) {
+        pillars[i].h = ROOM_HEIGHT;
+        pillars[i].r = 1;
+        pillars[i].shot = False;
+    }
+    pillars[0].pos.x = -ROOM_WIDTH/4;
+    pillars[0].pos.y = 0;
+    pillars[0].pos.z = 0;
+
+    pillars[1].pos.x = ROOM_WIDTH/4;
+    pillars[1].pos.y = 0;
+    pillars[1].pos.z = 0;
+
+    pillars[2].pos.x = -ROOM_WIDTH/4;
+    pillars[2].pos.y = 0;
+    pillars[2].pos.z = -ROOM_DEPTH/4;
+
+    pillars[3].pos.x = -ROOM_WIDTH/4;
+    pillars[3].pos.y = 0;
+    pillars[3].pos.z = ROOM_DEPTH/4;
+
+    pillars[4].pos.x = ROOM_WIDTH/4;
+    pillars[4].pos.y = 0;
+    pillars[4].pos.z = -ROOM_DEPTH/4;
+
+    pillars[5].pos.x = ROOM_WIDTH/4;
+    pillars[5].pos.y = 0;
+    pillars[5].pos.z = ROOM_DEPTH/4;
+}
+
+static void initSquare() {
+    float l1 = -0.5, l2 = 0.5;
+    float positions[] = {l1, l1, l2, l2, l1, l2, l1, l2, l2, l2, l1, l2, l2, l2, l2, l1, l2, l2,  // Frente
+                         l2, l1, l1, l1, l1, l1, l2, l2, l1, l1, l1, l1, l1, l2, l1, l2, l2, l1,  // Atrás
+                         l1, l1, l1, l1, l1, l2, l1, l2, l1, l1, l1, l2, l1, l2, l2, l1, l2, l1,  // Izquierda
+                         l2, l2, l1, l2, l2, l2, l2, l1, l1, l2, l2, l2, l2, l1, l2, l2, l1, l1,  // Derecha
+                         l1, l1, l1, l2, l1, l1, l1, l1, l2, l2, l1, l1, l2, l1, l2, l1, l1, l2,  // Abajo
+                         l2, l2, l1, l1, l2, l1, l2, l2, l2, l1, l2, l1, l1, l2, l2, l2, l2, l2   // Arriba
+    };
+
+    float normals[] = { 0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  // Frente
+                        0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  // Atrás
+                       -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0,  // Izquierda
+                        1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  // Derecha
+                        0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  // Abajo
+                        0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  // Arriba
+    };
+    float texcoords[] = {       0, 2,       0, 0,  2, 0,   2 , 0,   2 , 2,       0, 2,
+                           2 * 0.5, 2,  2 * 0.5, 0,       0, 0,        0, 0,        0, 2,  2 * 0.5, 2,
+
+                                0, 2,       0, 0,  2 * 0.5, 0,   2 * 0.5, 0,   2 * 0.5, 2,       0, 2,
+                           2 * 0.5, 2,  2 * 0.5, 0,       0, 0,        0, 0,        0, 2,  2 * 0.5, 2,
+
+                                0, 2,       0, 0,  2 * 0.5, 0,   2 * 0.5, 0,   2 * 0.5, 2,       0, 2,
+                           2 * 0.5, 2,  2 * 0.5, 0,       0, 0,        0, 0,        0, 2,  2 * 0.5, 2,
+    };
+
+    glUseProgram(programId1);
+    glGenVertexArrays(1, &targetVA);
+    glBindVertexArray(targetVA);
+    GLuint buffers[3];
+    glGenBuffers(3, buffers);
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    glVertexAttribPointer(vertexPositionLoc, 3, GL_FLOAT, 0, 0, 0);
+    glEnableVertexAttribArray(vertexPositionLoc);
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(normals), normals, GL_STATIC_DRAW);
+    glVertexAttribPointer(vertexNormalLoc, 3, GL_FLOAT, 0, 0, 0);
+    glEnableVertexAttribArray(vertexNormalLoc);
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[2]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(texcoords), texcoords, GL_STATIC_DRAW);
+    glVertexAttribPointer(vertexTexcoordLoc, 2, GL_FLOAT, 0, 0, 0);
+    glEnableVertexAttribArray(vertexTexcoordLoc);
+}
+
+static void initTargets() {
+    srand(time(NULL));
+    int i;
+    float xRange = ROOM_WIDTH/2 - 0.5;
+    float yRange = ROOM_HEIGHT/2 - 0.5;
+    float zRange = ROOM_DEPTH/2 - 0.5;
+    for(i = 0; i < N_TARGETS; i ++) {
+        targets[i].position.x = -xRange  + (xRange  + xRange)  * rand() / RAND_MAX;
+        targets[i].position.y = -yRange + (yRange + yRange) * rand() / RAND_MAX;
+        targets[i].position.z = -zRange  + (zRange  + zRange)  * rand() / RAND_MAX;
+        targets[i].shot = false;
+    }
+}
+
+static double distance2D(float x1, float x2, float y1, float y2) {
+    return sqrt((x1 - x2) * (x1 - x2) + (y1- y2) * (y1- y2));
+}
+
 static Bool collides(float x, float y, float z) {
     float startX = -ROOM_WIDTH / 2;
     float startY = -ROOM_HEIGHT / 2;
     float startZ = -ROOM_DEPTH / 2;
     float radius = 0.5;
     return   x - radius <= startX || x + radius >= startX + ROOM_WIDTH  ||
-             y - radius <= startY || y + radius >= startY + ROOM_HEIGHT ||
-             z - radius <= startZ || z + radius >= startZ + ROOM_DEPTH;
+    y - radius <= startY || y + radius >= startY + ROOM_HEIGHT ||
+    z - radius <= startZ || z + radius >= startZ + ROOM_DEPTH;
+}
+
+static int collidesPillar(int x, int z) {
+    for (int i = 0; i < N_PILLAR; i++) {
+        double d  = distance2D(x, pillars[i].pos.x, z, pillars[i].pos.z);
+        //printf("%.4lf <= %.4f\n", d, (observerR + pillars[i].r));
+        if ( d <= (observerR + pillars[i].r)) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 static void moveForward() {
     float yawRadians   = M_PI * observerYaw / 180;
     float incX = -observerSpeed * sin(yawRadians);
     float incZ = -observerSpeed * cos(yawRadians);
+
+    int idx = collidesPillar(observerX + incX, observerZ + incZ);
+    if (idx >= 0) {
+//    	double d1 = distance2D(observerX, pillars[idx].pos.x, observerZ, pillars[idx].pos.z); // Current
+//    	double d2 = distance2D(observerX + incX, pillars[idx].pos.x, observerZ + incZ, pillars[idx].pos.z); // New
+//    	printf("F: %.4lf, %.4lf\n", d1, d2);
+        return;
+    }
+
     if(collides(observerX + incX, observerY, observerZ + incZ)) {
         observerX -= incX;
         observerZ -= incZ;
@@ -352,6 +496,15 @@ static void moveBack() {
     float yawRadians   = M_PI * observerYaw / 180;
     float incX = observerSpeed * sin(yawRadians);
     float incZ = observerSpeed * cos(yawRadians);
+
+    int idx = collidesPillar(observerX + incX, observerZ + incZ);
+    if (idx >= 0) {
+//    	double d1 = distance2D(observerX, pillars[idx].pos.x, observerZ, pillars[idx].pos.z); // Current
+//    	double d2 = distance2D(observerX + incX, pillars[idx].pos.x, observerZ + incZ, pillars[idx].pos.z); // New
+//    	printf("F: %.4lf, %.4lf\n", d1, d2);
+        return;
+    }
+
     if(collides(observerX + incX, observerY, observerZ + incZ)) {
         observerX -= incX;
         observerZ -= incZ;
@@ -366,6 +519,15 @@ static void moveLeft() {
     float yawRadians   = M_PI * observerYaw / 180;
     float incX = observerSpeed * cos(yawRadians);
     float incZ = observerSpeed * sin(yawRadians);
+
+    int idx = collidesPillar(observerX - incX, observerZ + incZ);
+    if (idx >= 0) {
+//    	double d1 = distance2D(observerX, pillars[idx].pos.x, observerZ, pillars[idx].pos.z); // Current
+//    	double d2 = distance2D(observerX - incX, pillars[idx].pos.x, observerZ + incZ, pillars[idx].pos.z); // New
+//    	printf("L: %.4lf, %.4lf\n", d1, d2);
+        return;
+    }
+
     if(collides(observerX - incX, observerY, observerZ + incZ)) {
         observerX += incX;
         observerZ -= incZ;
@@ -380,6 +542,15 @@ static void moveRight() {
     float yawRadians   = M_PI * observerYaw / 180;
     float incX = observerSpeed * cos(yawRadians);
     float incZ = observerSpeed * sin(yawRadians);
+
+    int idx = collidesPillar(observerX + incX, observerZ - incZ);
+    if (idx >= 0) {
+//    	double d1 = distance2D(observerX, pillars[idx].pos.x, observerZ, pillars[idx].pos.z); // Current
+//    	double d2 = distance2D(observerX + incX, pillars[idx].pos.x, observerZ - incZ, pillars[idx].pos.z); // New
+//    	printf("R: %.4lf, %.4lf\n", d1, d2);
+        return;
+    }
+
     if(collides(observerX + incX, observerY, observerZ - incZ)) {
         observerX -= incX;
         observerZ -= incZ;
@@ -400,13 +571,11 @@ static void turnRight() {
     observerYaw -= angleSpeed;
 }
 
-// change
 static void turnDown() {
     if(angleSpeed > 30) angleSpeed = 0.15;
     if(observerPitch >= -70) observerPitch -= angleSpeed;
 }
 
-// change
 static void turnUp() {
     if(angleSpeed > 30) angleSpeed = 0.15;
     if(observerPitch <= 90)observerPitch += angleSpeed;
@@ -440,7 +609,7 @@ static void updateBulletPosition() {
         floatingAbs(bulletPosition[1]) <= ROOM_HEIGHT/2 &&
         floatingAbs(bulletPosition[2]) <= ROOM_DEPTH/2 ) {
         bulletInRange = 1;
-        //printf("%.2f, %.2f\n", floatingAbs(bulletPosition[0]), floatingAbs(bulletPosition[2]));
+//        printf("%.2f, %.2f\n", floatingAbs(bulletPosition[0]), floatingAbs(bulletPosition[2]));
     } else {
         bulletInRange = 0;
         shootActive = 0;
@@ -452,6 +621,8 @@ static void displayFunc() {
     mouseY = glutGet(GLUT_WINDOW_HEIGHT) / 2;
     glutWarpPointer(mouseX, mouseY);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    if(timer2sec == 0 && currentTargets <= N_TARGETS) currentTargets++;
 
     switch (motionType) {
         case FORWARD:       moveForward(); break;
@@ -484,43 +655,62 @@ static void displayFunc() {
     glBindTexture(GL_TEXTURE_2D, textures[1]);
     glDrawArrays(GL_TRIANGLES, 30,  6);
 
-    // Draw shotgun
-    glUseProgram(programId2);
-    glBindVertexArray(playerVA);
-    glBindTexture(GL_TEXTURE_2D, textures[2]);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
     // Draw cylinders
+//    glUseProgram(programId1);
+//    mIdentity(&modelMatrix);
+//    translate(&modelMatrix, -ROOM_WIDTH/4, 0, 0);
+//    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
+//    cylinder_draw(c);
+//
+//    mIdentity(&modelMatrix);
+//    translate(&modelMatrix, ROOM_WIDTH/4, 0, 0);
+//    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
+//    cylinder_draw(c);
+//
+//    mIdentity(&modelMatrix);
+//    translate(&modelMatrix, -ROOM_WIDTH/4, 0, -ROOM_DEPTH/4);
+//    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
+//    cylinder_draw(c);
+//
+//    mIdentity(&modelMatrix);
+//    translate(&modelMatrix, ROOM_WIDTH/4, 0, -ROOM_DEPTH/4);
+//    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
+//    cylinder_draw(c);
+//
+//    mIdentity(&modelMatrix);
+//    translate(&modelMatrix, -ROOM_WIDTH/4, 0, ROOM_DEPTH/4);
+//    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
+//    cylinder_draw(c);
+//
+//    mIdentity(&modelMatrix);
+//    translate(&modelMatrix, ROOM_WIDTH/4, 0, ROOM_DEPTH/4);
+//    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
+//    cylinder_draw(c);
+
     glUseProgram(programId1);
-    mIdentity(&modelMatrix);
-    translate(&modelMatrix, -ROOM_WIDTH/4, 0, 0);
-    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
-    cylinder_draw(c);
+    for (int i = 0; i < N_PILLAR; i++) {
+        mIdentity(&modelMatrix);
+        translate(&modelMatrix, pillars[i].pos.x, pillars[i].pos.y, pillars[i].pos.z);
+        glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
+        cylinder_draw(c);
+    }
 
-    mIdentity(&modelMatrix);
-    translate(&modelMatrix, ROOM_WIDTH/4, 0, 0);
-    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
-    cylinder_draw(c);
+    // Draw Targets
+    mIdentity(&viewMatrix);
+    glUseProgram(programId1);
+    rotateX(&viewMatrix, -observerPitch);
+    rotateY(&viewMatrix, -observerYaw);
+    translate(&viewMatrix, -observerX, -observerY, -observerZ);
+    glUniformMatrix4fv(viewMatrixLoc, 1, true, viewMatrix.values);
 
-    mIdentity(&modelMatrix);
-    translate(&modelMatrix, -ROOM_WIDTH/4, 0, -ROOM_DEPTH/4);
-    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
-    cylinder_draw(c);
-
-    mIdentity(&modelMatrix);
-    translate(&modelMatrix, ROOM_WIDTH/4, 0, -ROOM_DEPTH/4);
-    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
-    cylinder_draw(c);
-
-    mIdentity(&modelMatrix);
-    translate(&modelMatrix, -ROOM_WIDTH/4, 0, ROOM_DEPTH/4);
-    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
-    cylinder_draw(c);
-
-    mIdentity(&modelMatrix);
-    translate(&modelMatrix, ROOM_WIDTH/4, 0, ROOM_DEPTH/4);
-    glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
-    cylinder_draw(c);
+    for(int i = 0; i < currentTargets; i ++) {
+        mIdentity(&modelMatrix);
+        translate(&modelMatrix, targets[i].position.x, targets[i].position.y, targets[i].position.z);
+        glUniformMatrix4fv(modelMatrixLoc, 1, true, modelMatrix.values);
+        glBindVertexArray(targetVA);
+        glBindTexture(GL_TEXTURE_2D, textures[3]);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 
     //Draw light objects
     glUseProgram(programId3);
@@ -563,6 +753,13 @@ static void displayFunc() {
     glLineWidth(2.0);
     glDrawArrays(GL_LINES, 0, 4);
 
+
+    // Draw shotgun
+    glUseProgram(programId2);
+    glBindVertexArray(playerVA);
+    glBindTexture(GL_TEXTURE_2D, textures[2]);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
     glutSwapBuffers();
 }
 
@@ -574,8 +771,11 @@ static void reshapeFunc(int w, int h) {
     glUniformMatrix4fv(projectionMatrixLoc, 1, true, projectionMatrix.values);
 }
 
+
 static void timerFunc(int id) {
     glutTimerFunc(10, timerFunc, id);
+    if(timer2sec <= 2.0) timer2sec += 0.01;
+    else timer2sec = 0;
     glutPostRedisplay();
 }
 
@@ -601,16 +801,15 @@ static void keyPressedFunc(unsigned char key, int x, int y) {
 }
 
 static void mouseClick(int button, int state, int x, int y) {
-    printf("%d, %d\n", x, y);
+//    printf("%d, %d\n", x, y);
     if ( state != GLUT_UP) return;
     if(shootActive) return;
 
     if(button == GLUT_RIGHT_BUTTON) {
-        //puts("RIGHT - UP");
         // Norm coords
         float nx = 2.0 * x / glutGet(GLUT_WINDOW_WIDTH) - 1;
         float ny =  -1 * (2.0 * y / glutGet(GLUT_WINDOW_HEIGHT) - 1);
-        printf("Norm: %.2f, %.2f\n", nx, ny);
+//        printf("Norm: %.2f, %.2f\n", nx, ny);
 
         Vec4 rayN = {nx, ny, -1, 1};
         // View coords
@@ -618,7 +817,7 @@ static void mouseClick(int button, int state, int x, int y) {
         inverse(projectionMatrix, &invProjectioMatrix);
         Vec4 rayV;
         multiply(invProjectioMatrix, rayN, &rayV);
-        printf("View: %.2f, %.2f\n", rayV.x, rayV.y);
+//        printf("View: %.2f, %.2f\n", rayV.x, rayV.y);
 
         // World coords
         rayV.z = -1;
@@ -629,7 +828,7 @@ static void mouseClick(int button, int state, int x, int y) {
         multiply(invViewMatrix, rayV, &rayM);
         rayM.w = 0;
         vec4_normalize(&rayM);
-        printf("World: %.2f, %.2f, %.2f\n", rayM.x, rayM.y, rayM.z);
+//        printf("World: %.2f, %.2f, %.2f\n", rayM.x, rayM.y, rayM.z);
         // Set bullet initial position
         bulletPosition[0] = observerX;
         bulletPosition[1] = observerY;
@@ -654,7 +853,6 @@ static void mouseMotionFunc(int x, int y) {
     incY /= m;
     if(incX == 1.0) turnRight();
     else if(incX == -1.0) turnLeft();
-    // change
     if(incY == -1.0) turnUp();
     else if(incY == 1.0) turnDown();
     glutPostRedisplay();
@@ -664,7 +862,8 @@ int main2(int argc, char **argv) {
     setbuf(stdout, NULL);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowPosition(0 ,0);
+//    glutInitWindowPosition(0 ,0);
+//    glutInitWindowSize(900, 600);
     glutCreateWindow("Shooting Range App");
     glutFullScreen();
     glutDisplayFunc(displayFunc);
@@ -688,6 +887,9 @@ int main2(int argc, char **argv) {
     initRoom();
     initPlayer();
     initCross();
+    initPillars();
+    initSquare();
+    initTargets();
 
     glClearColor(0, 0, 0, 1.0);
     glutMainLoop();
